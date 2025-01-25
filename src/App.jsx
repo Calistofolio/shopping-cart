2c464d2188da383c8d7f3b5dc691e789e349100b
Luana Calisto <luanavenancio060@gmail.com>
2025-01-25 12:42:13 -0300

    implements product context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import Product from './components/product/product'
+import { ProductContext } from './contexts/productsContext'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState("melt")
 
   return (
     <div>
+      <ProductContext.Provider value={{count, setCount}}>
       <Product/>
+      </ProductContext.Provider>
     </div>
   )
 }
--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,15 +1,19 @@
 import { data } from "../../products-json/products.json"
+import { useContext } from "react"
 import Button from "../button/button"
+import { ProductContext } from "../../contexts/productsContext"
 import './product.css'
 
 function Product() {
+
+  const { count } = useContext(ProductContext)
 
   return (
     <div>
       {data.map((m) => 
       <div>
         <img src={m.image}/>
-        <h4>{m.title}</h4>
+        <h4>{count}</h4>
         <p>{m.value}</p>
         <Button/>
       </div>)}
