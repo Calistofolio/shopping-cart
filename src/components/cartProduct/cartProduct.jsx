d404c047b9395a95a28e6fd58db329b43d2c8734
Luana Calisto <luanavenancio060@gmail.com>
2025-01-27 00:45:48 -0300

    minor changes

--- src/components/cartProduct/cartProduct.jsx
+++ src/components/cartProduct/cartProduct.jsx
@@ -8,17 +8,15 @@ function CartProducts() {
   const { cartProducts } = useContext(ProductContext)
   return (
     <div>   
-      <li>
         {cartProducts.map((c)  => 
-        <ul key={c.value} id={c.value}>
             <div >  
                 <Checkbox price = {c.value}> 
                     <span> {c.title} </span>
                     <span className='productValue'>R$ {c.value} </span> 
                 </Checkbox>
             </div>
-          </ul>)}
-        </li>
+          )}
+        
     </div>
   )
 }
